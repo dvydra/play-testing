c8aeb512ca7826fdd6d36b2c02083936341570b5
Daniel Vydra <daniel.vydra@guardian.co.uk>
2009-10-30 23:25:51 +0000

    added capcha

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -2,8 +2,12 @@ package controllers;
 
 import models.Post;
 import play.Play;
+import play.cache.Cache;
 import play.data.validation.Required;
 import play.data.validation.Validation;
+import play.libs.Codec;
+import play.libs.Images;
+import play.libs.Images.Captcha;
 import play.mvc.*;
 
 import java.util.List;
@@ -24,17 +28,36 @@ public class Application extends Controller {
 
     public static void show(Long id) {
         Post post = Post.findById(id);
-        render(post);
+        String randomID = Codec.UUID();
+        render(post, randomID);
     }
 
-    public static void postComment(Long postId, @Required String author, @Required String content) {
+
+    public static void postComment(
+            Long postId,
+            @Required(message = "Author is required") String author,
+            @Required(message = "A message is required") String content,
+            @Required(message = "Please type the code") String code,
+            String randomID) {
         Post post = Post.findById(postId);
-        if (validation.hasErrors()) {
-            render("Application/show.html", post);
+        validation.equals(
+                code, Cache.get(randomID)
+        ).message("Invalid code. Please type it again");
+        if (Validation.hasErrors()) {
+            render("Application/show.html", post, randomID);
         }
         post.addComment(author, content);
         flash.success("Thanks for posting %s", author);
         show(postId);
     }
+
+
+    public static void captcha(String id) {
+        Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
+    }
+
 
 }
