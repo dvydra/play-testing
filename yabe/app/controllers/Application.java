80a42d4ceed0e5c2f259ded57e9d33c70874a4d8
Daniel Vydra <daniel.vydra@guardian.co.uk>
2009-10-30 22:16:51 +0000

    Added stylesheet

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import models.Post;
+import play.Play;
 import play.mvc.*;
 
 import java.util.List;
 
 public class Application extends Controller {
 
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
         List<Post> olderPosts = Post.find(
