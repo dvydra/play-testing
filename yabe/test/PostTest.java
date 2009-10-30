ab4809825939e28e1259f82151a06d9805f2e256
Daniel Vydra <daniel.vydra@guardian.co.uk>
2009-10-30 14:24:55 +0000

    Added Post to comment

--- yabe/test/PostTest.java
+++ yabe/test/PostTest.java
@@ -1,3 +1,4 @@
+import models.Comment;
 import models.Post;
 import models.User;
 import org.junit.Before;
@@ -43,5 +44,39 @@ public class PostTest extends UnitTest {
         assertEquals("My first post", firstPost.title);
         assertEquals("Hello world", firstPost.content);
         assertNotNull(firstPost.postedAt);
+    }
+
+    @Test
+    public void useTheCommentsRelation() {
+        // Create a new user and save it
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+
+        // Create a new post
+        Post bobPost = new Post(bob, "My first post", "Hello world").save();
+
+        // Post a first comment
+        bobPost.addComment("Jeff", "Nice post");
+        bobPost.addComment("Tom", "I knew that !");
+
+        // Count things
+        assertEquals(1, User.count());
+        assertEquals(1, Post.count());
+        assertEquals(2, Comment.count());
+
+        // Retrieve Bob's post
+        bobPost = Post.find("byAuthor", bob).first();
+        assertNotNull(bobPost);
+
+        // Navigate to comments
+        assertEquals(2, bobPost.comments.size());
+        assertEquals("Jeff", bobPost.comments.get(0).author);
+
+        // Delete the post
+        bobPost.delete();
+
+        // Check the all comments have been deleted
+        assertEquals(1, User.count());
+        assertEquals(0, Post.count());
+        assertEquals(0, Comment.count());
     }
 }
