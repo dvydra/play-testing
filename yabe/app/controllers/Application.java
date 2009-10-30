ae6e27be4e85702d864c036aab75e9e641905339
Daniel Vydra <daniel.vydra@guardian.co.uk>
2009-10-30 18:06:30 +0000

    adding frontpage template

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -1,11 +1,18 @@
 package controllers;
 
+import models.Post;
 import play.mvc.*;
+
+import java.util.List;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
 
 }
